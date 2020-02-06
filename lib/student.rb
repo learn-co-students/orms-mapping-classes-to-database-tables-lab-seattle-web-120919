e510b5f5222319ff0137a28cd224e7d92e5fba15
kkirby16 <kkirby16@my.whitworth.edu>
2020-02-05 18:11:20 -0800

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,41 +1,40 @@
 class Student
-
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(name, grade, id=nil)
-    @id = id 
-    @name = name 
+  def initialize(id = nil, name, grade)
+    @id = id
+    @name = name
     @grade = grade
-  end 
+  end
 
   def self.create_table
-    sql = <<-SQL 
+    sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY, 
-        name TEXT, 
-        grade INTEGER
-        )
-        SQL
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
     DB[:conn].execute(sql)
-  end 
+  end
 
   def self.drop_table
-    sql = "DROP TABLE students" 
+    sql = <<-SQL
+    DROP TABLE students;
+    SQL
     DB[:conn].execute(sql)
-  end 
+  end
 
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
-      VALUES (?, ?)
+    INSERT INTO students (name, grade)
+    VALUES (?,?)
     SQL
- 
+
     DB[:conn].execute(sql, self.name, self.grade)
 
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-
-    #However, at the end of your #save method, you do need to grab the ID of the last inserted row, i.e. the row you just inserted into the database, and assign it to the be the value of the @id attribute of the given instance.
   end
 
   def self.create(name:, grade:)
